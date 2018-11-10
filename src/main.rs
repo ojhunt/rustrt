ba29b801e42f377b174ed03535619d4b8947eadd
Oliver Hunt <oliver@apple.com>
2018-11-10 14:40:01 -0800

    Adding settings, etc

--- src/main.rs
+++ src/main.rs
@@ -79,14 +79,41 @@ fn load_model(path: &str) -> Scene {
 
 type ResultBufferType = (f64, usize);
 
+struct SceneSettings {
+    pub output_file: String,
+    pub scene_file: String
+
+}
+
+impl SceneSettings {
+    pub fn new() -> SceneSettings {
+        return SceneSettings {
+            output_file: String::new(),
+            scene_file: String::new()
+        };
+    }
+}
+
+fn load_settings() -> SceneSettings {
+    let commandline_yaml = load_yaml!("command_line.yml");
+    let matches = App::from_yaml(commandline_yaml).get_matches();
+    let output_file = matches.value_of("output").unwrap();
+    let scene_file = matches.value_of("scene").unwrap();
+    let mut settings = SceneSettings::new();
+    settings.output_file = output_file.to_string();
+    settings.scene_file = scene_file.to_string();
+    return settings;
+}
+
 // #[cfg(feature = "yaml")]
 fn main() {
-    let commandline_yaml = load_yaml!("command_line.yml");
-    
-    let matches = App::from_yaml(commandline_yaml).get_matches();
-println!("matches: {:?}", matches);
+
+    let settings = load_settings();
+
+
     const SIZE : usize = 700;
-    let scn = load_model("models/sponza.obj");
+    let scn = load_model(&settings.scene_file);
+
     let mut output = Box::new(image::RgbImage::new(SIZE as u32, SIZE as u32));
     let width = output.width() as f64;
     let height = output.height() as f64;
@@ -151,6 +178,6 @@ println!("matches: {:?}", matches);
         // *_pixel = image::Rgb([scaled_depth, scaled_intersection_count, scaled_node_count]);
         *_pixel = image::Rgb([scaled_depth,scaled_intersection_count,scaled_intersection_count]);
     }
-    output.save("image.png").unwrap();
+    output.save(settings.output_file).unwrap();
     println!("Done!");
 }
